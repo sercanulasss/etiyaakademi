aaed68bea226cfe4ea35921cea109100286dc000
sercanulasss <93444766+sercanulasss@users.noreply.github.com>
2023-04-06 16:12:27 +0300

    Add files via upload

---
+++ gecmenotu.py
@@ -0,0 +1,23 @@
+vize = int(input("sayi gir: ")) 
+final = int(input("sayi gir: ")) 
+ortalama = (vize * 0.4 ) + (final * 0.6) 
+if 0 < ortalama < 49: 
+    print(ortalama)
+    print("FF") 
+    
+if 50 < ortalama < 59:
+    print("DD") 
+
+if 60 < ortalama < 69:
+    print(ortalama)
+    print("CC") 
+
+if 70 < ortalama < 79:
+    print(ortalama)
+    print("BB") 
+
+if 80 < ortalama < 100:
+    print(ortalama)
+    print("AA")
+ 
+ 
