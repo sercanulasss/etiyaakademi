b8f6834668630f0078537b6be1a9d01fe7e905e9
sercanulasss <sercanulass@gmail.com>
2023-04-10 15:40:53 +0300

    pair4

---
+++ memo.py
@@ -0,0 +1,47 @@
+# ev = ["hansa","hasan","koray","memo","ulas"] 
+# for i in range(4):
+#     if (ev[i] == "memo"):
+#         ev[3] = "mehmetDuygu"
+        
+# print(ev)   
+
+
+#     #kaydırınca neden değişiyor?
+
+
+def memo():
+    fibo = [1,1] 
+
+    for i in range(18) :
+        toplam = fibo[i] + fibo[i + 1] 
+        fibo.append (toplam)
+    print(fibo)
+
+memo()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+    
+
+
+
+    
+
+
+    
+
+
