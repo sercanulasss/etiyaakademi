24bf1004328c7af7d5e132c3bf22fcfc72716a03
sercanulasss <sercanulass@gmail.com>
2023-04-10 10:33:00 +0300

    function files are added.

---
+++ functions/hesap makinesi.py
@@ -0,0 +1,27 @@
+def hesapmaki():
+    sayi1 = int(input("sayi gir: "))
+    sayi2 = int(input("sayi gir: ")) 
+    islemsec = (input("islem: ")) 
+
+    if (islemsec == "*") :
+     print(sayi1 * sayi2)
+
+
+    elif (islemsec == "+"):
+     print(sayi1 + sayi2)
+
+
+    elif (islemsec == "/") and (sayi2 == 0 ):
+     print("hesaplanamadi") 
+    elif ( islemsec == "/"): 
+     print(sayi1 / sayi2)
+
+
+    elif (islemsec == "-"):
+     print(sayi1 - sayi2) 
+
+    else:
+     print("gecerli deger yok")  
+
+hesapmaki()
+
